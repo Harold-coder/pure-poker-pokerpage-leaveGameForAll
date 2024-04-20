17878910a486f0e80eb02d8b05781b504985ed30
Harold-coder <hjc2154@columbia.edu>
2024-04-20 01:35:03 -0400

    Now updating chips as well

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const mysql = require('mysql');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const gameTableName = process.env.GAME_TABLE;
 
@@ -15,6 +16,11 @@ exports.handler = async (event) => {
         }
 
         if (!game.gameInProgress) {
+            const promises = game.players.map(player => 
+                updateChipsBalance(player.id, player.chips));
+    
+            // Wait for all updateChips invocations to complete
+            await Promise.all(promises);
             await deleteGame(gameId);
 
             return { statusCode: 200, body: JSON.stringify({ message: 'Game is deleted!' }) };
@@ -47,3 +53,57 @@ async function getGameState(gameId) {
     const { Item } = await dynamoDb.get(params).promise();
     return Item;
 }
+
+
+const updateChipsBalance = async (playerId, buyIn) => {
+    return new Promise((resolve, reject) => {
+        const connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+
+        connection.connect(err => {
+            if (err) {
+                console.error('Database connection failed:', err);
+                reject(new Error('Database connection failed', host, user, password, databse));
+                return;
+            }
+
+            const playerChipsQuery = 'SELECT chips FROM users WHERE username = ?';
+            connection.query(playerChipsQuery, [playerId], (err, results) => {
+                if (err) {
+                    connection.end();
+                    reject(new Error(`Failed to fetch player chips. Error: ${err.message}`));
+                    return;
+                }
+
+                if (results.length === 0) {
+                    connection.end();
+                    reject(new Error(`Player not found. Searched for playerId: '${playerId}'`));
+                    return;
+                }
+
+                const currentChips = results[0].chips;
+                const newChipsAmount = currentChips + buyIn;
+
+                if (newChipsAmount < 0) {
+                    connection.end();
+                    reject(new Error('Not enough chips'));
+                    return;
+                }
+
+                const updateQuery = 'UPDATE users SET chips = ? WHERE username = ?';
+                connection.query(updateQuery, [newChipsAmount, playerId], (err) => {
+                    connection.end();
+                    if (err) {
+                        reject(new Error(`Failed to update chips balance. Error: ${err.message}`));
+                        return;
+                    }
+                    resolve(true);
+                });
+            });
+        });
+    });
+};
